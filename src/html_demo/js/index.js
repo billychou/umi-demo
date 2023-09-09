26ac898a55f2b13365e41fb4504c30f8b76344fc
billychou <zsc1528@gmail.com>
2023-09-10 00:14:59 +0800

    guess field

--- src/html_demo/js/index.js
+++ src/html_demo/js/index.js
@@ -1,3 +1,39 @@
+const formGuessField = document.querySelector(".guessField");
+const formSubmit = document.querySelector(".guessSubmit");
+
+// result paragraph
+const resultGuess  = document.querySelector(".resultGuess");
+const resultHighOrLow = document.querySelector(".resultHighOrLow");
+const demo = document.querySelector(".demo");
+
+
+const randomValue = Math.floor(Math.random() * 100);
+
+/**
+ * 提交后check事件
+ */
+function guessCheck() {
+    const guessValue = formGuessField.value;
+    if (guessValue > randomValue) {
+        alert("太大了");
+    } else if (guessValue < randomValue) {
+        alert("太小了");
+    } else {
+        alert("You Got It");
+        disableGusss();
+    }
+}
+
+/**
+ * guess disable
+ */
+function disableGusss() {
+    formGuessField.disabled = true;
+}
+
+console.log(formGuessField);
+console.log(formSubmit);
+formSubmit.addEventListener("click", guessCheck);
 
 function handleOnClick() {
     
