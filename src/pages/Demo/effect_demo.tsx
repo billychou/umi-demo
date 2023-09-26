788a0ff661942526a0529be3fae02339d78b7d84
billychou <zsc1528@gmail.com>
2023-09-26 23:45:21 +0800

    effect

--- src/pages/Demo/effect_demo.tsx
+++ src/pages/Demo/effect_demo.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Card } from 'antd';
+import { transformKeySubmitValue } from "@ant-design/pro-components";
 
 const EffectDemo: React.FC = () => {
     const [number, setNumber]  = useState(0);
@@ -8,75 +9,78 @@ const EffectDemo: React.FC = () => {
         name: "",
         selected: false,
     });
-    console.count("render");
 
     /**
      * Case1: 连接到聊天服务器
      */
-    useEffect(()=>{
-        const githubUrl = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-        const fetchPromise = fetch(githubUrl);
-        console.log(fetchPromise);
-        fetchPromise.then(res => {
-            console.log(`收到相应: ${res.status}`);
-        })
-        console.log("已发送请求...");
-    }, []);
+    // useEffect(()=>{
+    //     const githubUrl = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+    //     const fetchPromise = fetch(githubUrl);
+    //     console.log(fetchPromise);
+    //     fetchPromise.then(res => {
+    //         console.log(`收到相应: ${res.status}`);
+    //     })
+    //     console.log("已发送请求...");
+    // }, []);
 
 
     /**
      * Case2: 监听全局浏览器事件 
      */
-    useEffect(()=>{
-       function handleMove(e: any) {
-           setPosition({x: e.clientX, y: e.clientY});
-       } 
-       window.addEventListener('pointermove', handleMove);
-       return () => {
-           window.removeEventListener('pointermove', handleMove);
-       }
-    }, []);
+    // useEffect(()=>{
+    //    function handleMove(e: any) {
+    //        setPosition({x: e.clientX, y: e.clientY});
+    //    } 
+    //    window.addEventListener('pointermove', handleMove);
+    //    return () => {
+    //        window.removeEventListener('pointermove', handleMove);
+    //    }
+    // }, []);
 
     /**
      * Case3: 触发动画效果
      * 
      */
-    useEffect(()=>{
+    // useEffect(()=>{
 
+    // }, []);
+    useEffect(()=>{
+        const fruits = [];
+        fruits.push("banana", "apple", "peach");
+        console.log(fruits.length);
+        fruits[5] = "mango";
+        console.log(Object.keys(fruits));
+        console.log(fruits.length);
+        fruits.length = 10;
+        console.log(fruits);
+        console.log(fruits.length);
+        console.log(fruits[8]);
+
+        const arrayEmpty = new Array(2);
+        console.log(arrayEmpty.length);
+        console.log(0 in arrayEmpty);
+        console.log(1 in arrayEmpty);
+        const arrayOfOne = new Array("2");
+        console.log(arrayOfOne.length);
+        console.log(arrayOfOne[0]);
+
+        const myFruits = new Array("Apple", "Bannana");
+        console.log(myFruits.length);
+        console.log(myFruits[0]);
+
+        var keys = [];
+        for (const i of fruits) {
+            console.log(i);
+        }
+        
     }, []);
 
     return (
         <>
-            <h1 style={{
-                opacity: 0,
-                color: 'white',
-                padding: 50,
-                textAlign: 'center',
-                fontSize: 50,
-                backgroundImage: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)'
-                }}
-            >
+            <h1> 
                 Welcome
             </h1>
-            {/* <Card title="global_register">        
-                <div style={{
-                    position: 'absolute',
-                    backgroundColor: 'red',
-                    borderRadius: '50%',
-                    opacity: 0.6,
-                    transform: `translate(${position.x}px, ${position.y}px)`,
-                    pointerEvents: 'none',
-                    left: -20,
-                    top: -20,
-                    width: 40,
-                    height: 40,
-                }}>
-                </div>
-            </Card> */}
-
         </>
-
-
     );
 };
 
