a6a4bde5d5b38c919055c09cb66663963ac679f0
billychou <zsc1528@gmail.com>
2023-12-26 15:11:35 +0800

    增加react-router

--- react-tutorial/react-router-demo/src/main.jsx
+++ react-tutorial/react-router-demo/src/main.jsx
@@ -7,8 +7,19 @@ import {
 import App from './App.jsx';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>hello</div>,
+  },
+  {
+    path: 'app',
+    element: <App />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider  router={router} />
   </React.StrictMode>,
 )
