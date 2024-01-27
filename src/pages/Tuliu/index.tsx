e9c5159021edb26864762f95264e37000dda10db
billychou <zsc1528@gmail.com>
2024-01-27 19:33:06 +0800

    无序列表

--- src/pages/Tuliu/index.tsx
+++ src/pages/Tuliu/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-
+import { Table, type TableColumnsType } from 'antd';
 import Header from './components/Header';
+
+const columns: TableColumnsType = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+    }
+];
 
 const TuliuIndex = () => {
     return (
